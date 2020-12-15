18d46fc69ff94292a27b236df7564e41e31e4e8d
Steve Freeland <stevefreeland1@gmail.com>
2020-12-15 18:24:17 -0500

    replaced API key with 'enter your API key here' statement in config.js and logic.js statements

--- static/js/logic.js
+++ static/js/logic.js
@@ -46,14 +46,14 @@ function createMap(earthquakes) {
         attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
         maxZoom: 18,
         id: "mapbox.light",
-        accessToken: "pk.eyJ1Ijoic3RldmVmcmVlbGFuZCIsImEiOiJja2lwZ3h5MTUwZjdmMnNycTJoc3pvYmEwIn0.wiSmLoYM0r_FyzLf3dWslQ"
+        accessToken: "your API key here"
     });
 
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
         maxZoom: 18,
         id: "mapbox.light",
-        accessToken: "pk.eyJ1Ijoic3RldmVmcmVlbGFuZCIsImEiOiJja2lwZ3h5MTUwZjdmMnNycTJoc3pvYmEwIn0.wiSmLoYM0r_FyzLf3dWslQ"
+        accessToken: "your API key here"
     });
  
     var baseMaps = {
